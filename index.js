388285ef0cc43cbcd1fd6b2eecb6a07210091584
nihelouanassi <nihelouanasi@gmail.com>
2024-02-15 15:51:36 +0100

    add to do

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-function sayHello(){
-   return "hello world";
-}
+const express = require('express');
+const app = express();
+const port = 3000;
 
-module.exports = sayHello;
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let tasks = ['Task 1', 'Task 2', 'Task 3'];
+
+app.get('/', (req, res) => {
+  res.render('index', { tasks });
+});
+
+app.post('/addTask', (req, res) => {
+  const newTask = req.body.task;
+  tasks.push(newTask);
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
