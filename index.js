494034692b6b4ca9bd7954546e6ad26910fa8090
Tania Olivia <tania.olivia69@yahoo.com>
2024-02-15 15:45:17 +0100

    add calculator

--- index.js
+++ index.js
@@ -6,4 +6,87 @@ function tania(){
    return "Hey ! It's the basic Tania ;)";
 }
 
-module.exports = { sayHello, tania };
+const readline = require('node:readline').createInterface({
+   input: process.stdin,
+   output: process.stdout,
+ });
+
+function startCalculation(name) {
+   readline.question(`\nAlright ! What type of equation do you want to use (addition, substraction, multiplication, division) ? \n`, equation => {
+      let chosenEquation = "";
+
+      readline.question(`\nNext, what is your first number ? \n`, num1 => {
+         readline.question(`\nLastly, what is your second number ? \n`, num2 => {
+            let answer = 0; 
+            const firstNumber = parseFloat(num1);
+            const secondNumber = parseFloat(num2);
+
+            if(equation === "addition") {
+               chosenEquation = "+";
+               answer = firstNumber + secondNumber;
+             }
+             else if(equation === "substraction") {
+               chosenEquation = "-";
+               answer = firstNumber - secondNumber;
+             }
+             else if(equation === "multiplication") {
+               chosenEquation = "*";
+               answer = firstNumber * secondNumber;
+             }
+             else if(equation === "division") {
+               chosenEquation = "/";
+               answer = firstNumber / secondNumber;
+             }
+
+            console.log(`\nOkay, i've counted it for you. The answer of ${firstNumber} ${chosenEquation} ${secondNumber} is ${answer}`)
+
+            readline.question(`\nDo you want to calculate other numbers ? (yes, no) \n`, continueCalculation => {
+               if(continueCalculation === "yes") {
+                  startCalculation();
+               }
+               else {
+                  console.log(`\nOkay ! See you next time, ${name} :)`);
+                  readline.close();
+               }
+            });
+         });
+      });
+  });
+}
+
+function getName(name) {
+   readline.question(`\nHi ${name}! Do you want to start the counting now ? (yes, no) \n`, start => {
+         if(start === "yes") {
+            startCalculation(name);
+         }
+         else {
+            console.log(`\nSee you next time, ${name} :)`);
+            readline.close();
+         }   
+   });
+}
+
+function demandName() {
+   readline.question(`\nSorry but i need your name please :) So, what's your name ? If you want to quit, write "quit" \n`, answer => {
+      if(answer === "quit") {
+         console.log(`\nSee you next time, stranger :)`);
+         readline.close();
+      }
+      else {
+         answer !== "" ? getName(answer) : demandName();
+      }
+   });
+}
+
+function calculator() {
+    readline.question(`Welcome to the Calculator World ! Who am i talking to right now ? \n`, name => { 
+      if(name !== "") {
+        getName(name);
+      }
+      else {
+        demandName();
+      }
+    });
+}
+
+module.exports = { sayHello, tania, calculator };
